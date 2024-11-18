19e2206cc09582c62641ace2a7d4f4fdae33aaa2
Ramon Diaz <r.diaz@intellisys.com.do>
2024-11-18 09:10:47 -0400

    creating rent page

---
+++ src/app/rent/page.js
@@ -0,0 +1,65 @@
+"use client";
+
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+// import RentForm from "../components/RentForm";
+import AppbarTest from "../components/AppbarTest"
+import Footer from "../components/Footer";
+import RentTable from "../components/RentTable";
+
+export default function Rent() {
+  const [data, setData] = useState([]);
+  const router = useRouter();
+  const goToDashboard = () => {
+    router.push("/dashboard");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        router.push("/");
+        return;
+      }
+
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+
+      if (decodedToken.exp < currentTime) {
+        localStorage.removeItem("token");
+        router.push("/login");
+        return;
+      }
+
+      try {
+        const res = await fetch("https://rentcar-backend.onrender.com/api/rent", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, [router]);
+
+  return (
+    <div>
+      <AppbarTest/>
+      <div className="" style={{ textAlign: "center", padding: "50px" }}>
+        {/* <RentForm /> */}
+        {data.length > 0 ? <RentTable rent={data} /> : <p>No rents found.</p>}
+      </div>
+      <Footer/>
+    </div>
+  );
+}
