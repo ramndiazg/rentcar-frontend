1dc0e123dd4d327b6237b4b62b1d80421734813f
Ramon Diaz <r.diaz@intellisys.com.do>
2024-11-25 16:08:04 -0400

    fixing servide page

---
+++ src/app/services/page.js
@@ -0,0 +1,88 @@
+"use client";
+
+import * as React from "react";
+import { styled } from "@mui/system";
+import Button from "@mui/material/Button";
+import AppbarTest from "../components/AppbarTest";
+import Footer from "../components/Footer";
+
+const StyledContainer = styled("div")({
+  textAlign: "center",
+  padding: "50px",
+  backgroundColor: "black",
+  color: "#f5f5f5",
+});
+
+const StyledTitle = styled("h1")({
+  fontSize: "2.5rem",
+  marginBottom: "20px",
+});
+
+const StyledParagraph = styled("p")({
+  fontSize: "1.2rem",
+  lineHeight: "1.8",
+  marginBottom: "30px",
+});
+
+const StyledEmployeeList = styled("ul")({
+  listStyle: "none",
+  padding: 0,
+  marginBottom: "40px",
+});
+
+const StyledEmployeeItem = styled("li")({
+  fontSize: "1.1rem",
+  marginBottom: "10px",
+});
+
+const StyledSocialLinks = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  gap: "15px",
+});
+
+const StyledButton = styled(Button)({
+  backgroundColor: "#01579b",
+  color: "white",
+  "&:hover": {
+    backgroundColor: "#01406e",
+  },
+});
+
+export default function AboutPage() {
+  return (
+    <div>
+      <AppbarTest />
+      <StyledContainer>
+        <StyledTitle>Services</StyledTitle>
+        <StyledParagraph>
+          Welcome to RentCar App, soon we have news services.
+        </StyledParagraph>
+        <StyledSocialLinks>
+          <StyledButton
+            variant="contained"
+            href="https://facebook.com"
+            target="_blank"
+          >
+            Facebook
+          </StyledButton>
+          <StyledButton
+            variant="contained"
+            href="https://instagram.com"
+            target="_blank"
+          >
+            Instagram
+          </StyledButton>
+          <StyledButton
+            variant="contained"
+            href="https://wa.me/1234567890"
+            target="_blank"
+          >
+            WhatsApp
+          </StyledButton>
+        </StyledSocialLinks>
+      </StyledContainer>
+      <Footer />
+    </div>
+  );
+}
